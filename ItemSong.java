3fabdc7b35c7334d8b22a3f55d087bfa68010cff
Oscaarjs <37636054+Oscaarjs@users.noreply.github.com>
2018-05-08 17:24:35 +0200

    Update ItemSong.java

--- ItemSong.java
+++ ItemSong.java
@@ -1,33 +1,38 @@
 
- 
+package Proj6;
+
 public class ItemSong {
- 
-    private double songLength;
-    private String songTitle;
-    private String artist;
-    private String songFilePath;
- 
-    public ItemSong(String artist, String songTitle, String songFilePath, double songLength) {
- 
-        this.artist = artist;
-        this.songTitle = songTitle;
-        this.songFilePath = songFilePath;
-        this.songLength = songLength;
-    }
- 
-    public double getSongLength() {
-        return songLength;
-    }
- 
-    public String getSongTitle() {
-        return songTitle;
-    }
- 
-    public String getArtistName() {
-        return artist;
-    }
- 
-    public String toString() {
-        return artist + " - " + songTitle + " - " + songLength;
-    }
+
+	private double songLength;
+	private String songTitle;
+	private String artist;
+	private String songFilePath;
+
+	public ItemSong(String artist, String songTitle, String songFileName, double songLength) {
+
+		this.artist = artist;
+		this.songTitle = songTitle;
+		this.songFilePath = songFileName;
+		this.songLength = songLength;
+	}
+
+	public double getSongLength() {
+		return songLength;
+	}
+
+	public String getSongTitle() {
+		return songTitle;
+	}
+
+	public String getArtistName() {
+		return artist;
+	}
+	
+	public String getSongFilePath() {
+		return songFilePath;
+	}
+
+	public String toString() {
+		return artist + " - " + songTitle;
+	}
 }
