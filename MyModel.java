ca3abf544c0a113473e21967898de08373f74c27
Oscaarjs <37636054+Oscaarjs@users.noreply.github.com>
2018-05-08 17:27:01 +0200

    Add files via upload

---
+++ MyModel.java
@@ -0,0 +1,25 @@
+package Proj6;
+
+import javax.swing.AbstractListModel;
+
+public class MyModel extends AbstractListModel {
+	private String[] items;
+
+	public MyModel(String[] items) {
+		this.items = items;
+	}
+
+	@Override
+	public Object getElementAt(int index) {
+		return items[index];
+	}
+
+	@Override
+	public int getSize() {
+		return items.length;
+	}
+
+	public void update() {
+		this.fireContentsChanged(this, 0, items.length - 1);
+	}
+}
