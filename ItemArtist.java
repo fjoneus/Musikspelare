10bb3cbfb4012d8a00c19cbde54603f9dc40e2c7
Adellingsen <38975882+Adellingsen@users.noreply.github.com>
2018-05-14 20:46:20 +0200

    Add files via upload

--- ItemArtist.java
+++ ItemArtist.java
@@ -8,21 +8,38 @@ public class ItemArtist {
 	public  ArrayList<ItemSong> theArrayList;
 	private String ArtistName;
 	
-	public ItemArtist(String s, String a, ItemSong x) {
-		songs = s;
-		ArtistName = a;
+	
+	/**
+	 * This constructs an ItemArtist witch contains two strings, on for song and the other one for artistname
+	 * It also contains an ItemSong witch atomaticly is added to an ArrayList  
+	 * @param song, The name of the song
+	 * @param artist, The name of the artist
+	 * @param x, An ItemSong(See ItemSong docs for details)
+	 */
+	public ItemArtist(String song, String artist, ItemSong x) {
+		songs = song;
+		ArtistName = artist;
 		theArrayList = new ArrayList<ItemSong>();
 		theArrayList.add(x);
 		
 	}
-	
+	/**
+	 * This returns an artistname
+	 * @return the namen of the artist
+	 */
 	public String getArtist() {
 		return ArtistName;
 	}
+	/**
+	 * This return a song of an artist
+	 * @return the songname 
+	 */
     public String getArtistSongs() {
     	return songs;	
     }
-    
+    /**
+     * Make a string of a ItemArtist, witch contains an artistname and a songname
+     */
     public String toString() {
 		return "----------------\n"+ ArtistName + " Tracks: "+ "\n\n"+ songs + "\n----------------\n";
     	
--- ItemSong.java
+++ ItemSong.java
@@ -7,6 +7,13 @@ public class ItemSong {
 	private String artist;
 	private String songFilePath;
 
+	/**
+	 * This constructs a ItemSong witch contains an artist, songTitle, songFileName and songlength 
+	 * @param artist, the name of the artist
+	 * @param songTitle, the title of the song
+	 * @param songFileName, the name of the songfile
+	 * @param songLength, the length of the song
+	 */
 	public ItemSong(String artist, String songTitle, String songFileName, double songLength) {
 
 		this.artist = artist;
@@ -14,23 +21,40 @@ public class ItemSong {
 		this.songFilePath = songFileName;
 		this.songLength = songLength;
 	}
-
+	/**
+	 * This returns the songlength
+	 * @return this length of the song in the ItemSong
+	 */
 	public double getSongLength() {
 		return songLength;
 	}
-
+	
+	/**
+	 *	This returns the songtitle
+	 * @return This title of the song in the ItemSong
+	 */
 	public String getSongTitle() {
 		return songTitle;
 	}
 
+	/**
+	 *	This returns the artists name
+	 * @return This name of the artist in the ItemSong
+	 */
 	public String getArtistName() {
 		return artist;
 	}
 	
+	/**
+	 *	This returns the name of the file to the song
+	 * @return The name of the file
+	 */
 	public String getSongFilePath() {
 		return songFilePath;
 	}
-
+	/**
+	 * This make it a string With only artist and songtitle
+	 */
 	public String toString() {
 		return artist + " - " + songTitle;
 	}
